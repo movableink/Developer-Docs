517993491022a2c372b609131016496c50a1633b
Brett Mascavage <savagecorp@gmail.com>
2014-10-17 11:48:29 -0400

    cleaned up JS / styled an example page / added back to index links

--- js/build/production.js
+++ js/build/production.js
@@ -19,25 +19,27 @@ $(function() {
     });
 });
 
-
-// Slide Toggle Content Sections
+// SlideToggle FAQ content interaction
 $('.faq-trigger').click(function(){
-    $('.faq-trigger').not(this).find('div').slideUp('fast');
-    $(this).children().slideToggle("fast");
+    event.preventDefault();
+    $(this).next('.faq-content').slideToggle(500).end().parent('li').toggleClass('content-visible');
 
-    // add active states to sidebar
+    // add active states to sidebar when a question is clicked in a category
     $('.active').removeClass('active');
     var id = $(this).attr('id');
     $('[href="#' + id + '"]').addClass('active');
+
 });
 
 
+// Add active class to categories in the sidebar when clicked on
 $(".faq-categories li a").click(function() {
   $(".faq-categories li").not(this).find('a').removeClass("active");
   $(this).toggleClass("active");
 });
 
 
+// Smooth scrolling
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -52,8 +54,3 @@ $(function() {
     }
   });
 });
-
-
-
-
-
